f13a14fd83f131116abd8d31adcec0ab3eb4e98e
tikegami-tak <129657447+tikegami-tak@users.noreply.github.com>
2023-04-28 16:49:09 +0900

    Add files via upload

---
+++ Measurements_on_500_BBO/62_ofs3000_pw100_40ms_313K_DMSO_CW/prep.sh
@@ -0,0 +1,14 @@
+#!/bin/csh
+
+set pdir = "../61_ofs3000_pw100_40ms_313K_DMSO_AFTAC"
+
+mkdir process pick_area
+
+cp $pdir/sep*pl ./
+
+cp $pdir/process/*com process/
+cp $pdir/process/*pl process/
+
+cp $pdir/pick_area/*pl pick_area/
+cp $pdir/pick_area/indoxylsulfate_shift.txt pick_area/
+cp $pdir/pick_area/indoxylsulfate_shift_fold2.txt pick_area/
